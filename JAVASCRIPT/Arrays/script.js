77ab0a49d04df53aa92af30958b8779abcfbcd93
AryanJaiswalDev <aryan.j0486@gmail.com>
2026-09-09 12:03:17 +0530

    loops and string

---
+++ JAVASCRIPT/Arrays/script.js
@@ -0,0 +1,12 @@
+//by object form 
+let marks={
+    stud1:98,
+    stud2:23,
+}
+//ARRAY (linear way to store)
+let markss=[12,34,56,78,90,111];
+let heroes=["aryan",'superman',"ironman"];
+let info=["rahul",98,'a'];
+console.log(markss);
+console.log(info);
+console.log(heroes);
---
+++ JAVASCRIPT/Strings/Basics/script.js
@@ -0,0 +1,6 @@
+let s="aryan jaiswal";
+let s2='aaryan';
+console.log(s.length)//string length
+
+console.log(s[0])
+
