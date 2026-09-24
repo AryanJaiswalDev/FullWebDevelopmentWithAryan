f23e9a020140d6ff7d191814b0f55c95d1c5b01f
AryanJaiswalDev <aryan.j0486@gmail.com>
2026-09-24 18:51:38 +0530

    dom

--- JAVASCRIPT/DOM/script.js
+++ JAVASCRIPT/DOM/script.js
@@ -1,72 +1,103 @@
-// // console.log("hello");
-// // alert("kya kaam hai ? ");
-// // //windows objects 
+// // // console.log("hello");
+// // // alert("kya kaam hai ? ");
+// // // //windows objects 
 
 
 
 
-// // //it is a global object with lots of properties and methods and is automaticllay crrated by browser its a browser object not js objects 
+// // // //it is a global object with lots of properties and methods and is automaticllay crrated by browser its a browser object not js objects 
 
 
-// // window.console.log("hello2");
+// // // window.console.log("hello2");
 
-// // // DOM 
-// // // Document object model 
-// // // when a page is loaded, the browser creates a dom of the page 
-// // // console.dir --> prints object 
-// // //html lka sara code js me accsess kiya  ja skta hai aur js me vo ek object ke form me jata hai ussi object ko DOM bolte hain .
+// // // // DOM 
+// // // // Document object model 
+// // // // when a page is loaded, the browser creates a dom of the page 
+// // // // console.dir --> prints object 
+// // // //html lka sara code js me accsess kiya  ja skta hai aur js me vo ek object ke form me jata hai ussi object ko DOM bolte hain .
 
-// // // WINDOW--->DOCUMENT--->HTML--->HEAD & BODY
+// // // // WINDOW--->DOCUMENT--->HTML--->HEAD & BODY
 
-// //console.dir(document.body);
-// console.log(document.body);
-// console.log(document.head);
-// console.dir(document.body.childNodes[1]);
+// // //console.dir(document.body);
+// // console.log(document.body);
+// // console.log(document.head);
+// // console.dir(document.body.childNodes[1]);
 
-// //dynamic changes by user in console thats is why dom useing 
-// document.body.childNodes[1].innerText="abcdef";
+// // //dynamic changes by user in console thats is why dom useing 
+// // document.body.childNodes[1].innerText="abcdef";
 
-//dom manipulation
-// 1st--> select with id 
-let heading=document.getElementById("headingid");//return value of h1 
-console.dir(heading);
+// //dom manipulation
+// // 1st--> select with id 
+// let heading=document.getElementById("headingid");//return value of h1 
+// console.dir(heading);
 
-// 2nd--> select via class
-let classs=document.getElementsByClassName("q");//return collectios 
-console.dir(classs);
-console.log(classs);
+// // 2nd--> select via class
+// let classs=document.getElementsByClassName("q");//return collectios 
+// console.dir(classs);
+// console.log(classs);
 
-// 3rd--> select via tag
-let para=document.getElementsByTagName("p");
-console.dir(para);
-console.log(para);
+// // 3rd--> select via tag
+// let para=document.getElementsByTagName("p");
+// console.dir(para);
+// console.log(para);
 
-//QUERY SELECTOR --> return node list 
-let firstelements=document.querySelector("p");//one element
-console.dir(firstelements);//via tag
+// //dom manipulation attributes 
 
-let allelements=document.querySelectorAll("p");//all elements 
-console.dir(allelements);
-
-let classele=document.querySelectorAll(".h2");
-console.dir(classele);//via class 
-
-let accsessbyid=document.querySelector("#headingid");
-console.debug(accsessbyid);//via id 
-
-//properties 
-//> tagname:return tage for element Node
-//> innerText:returns the text contemt of the element and its Children
-//> innerHTML: return the plain text or html cpntents int the element 
-//> textContent:returns textual content even for hidden elements 
-//check in console firstelements.tag --> p will output as its is paragraph 
-
-//DOM tree contains three type of trees
-//>text nodes
-//>comment nodes
-//>element nodes (most used)
-
-//study about first child,last child div.childern[0]....
+//getAttribute(attr)//to get the attribute value
+//setAttribute(atrr,value)//to set the attribute with value
 
 let div=document.querySelector("div");
-console.dir(div);
+console.log(div);
+let id=div.getAttribute("id");
+console.log(id);//give id name line number 30 in html 
+let name=div.getAttribute("name");
+console.log(name);
+
+
+// //QUERY SELECTOR --> return node list 
+// let firstelements=document.querySelector("p");//one element
+// console.dir(firstelements);//via tag
+
+// let allelements=document.querySelectorAll("p");//all elements 
+// console.dir(allelements);
+
+// let classele=document.querySelectorAll(".h2");
+// console.dir(classele);//via class 
+
+// let accsessbyid=document.querySelector("#headingid");
+// console.debug(accsessbyid);//via id 
+
+// //properties 
+// //> tagname:return tage for element Node
+// //> innerText:returns the text contemt of the element and its Children
+// //> innerHTML: return the plain text or html cpntents int the element 
+// //> textContent:returns textual content even for hidden elements 
+// //check in console firstelements.tag --> p will output as its is paragraph 
+
+// //DOM tree contains three type of trees
+// //>text nodes
+// //>comment nodes
+// //>element nodes (most used)
+
+// //study about first child,last child div.childern[0]....
+
+// let div=document.querySelector("div");
+// console.dir(div);
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText="abc";
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText+" appending using innertext property";
+
+// let divs=document.querySelectorAll(".box");
+// // console.dir(divs[0]);
+// divs[0].innerText="new value";
+
+// let idx=1;
+// for(div of divs){
+//     div.innerText='new value unique ${idx}';
+//     idx++;
+// }
